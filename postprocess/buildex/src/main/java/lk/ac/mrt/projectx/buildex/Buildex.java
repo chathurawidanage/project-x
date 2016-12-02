7a8e561f020e951593e03bc729f6bbebd0f9bf9e
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2016-12-02 18:03:22 +0530

    Logging enabled.

---
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/Buildex.java
@@ -0,0 +1,18 @@
+package lk.ac.mrt.projectx.buildex;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * @author Chathura Widanage
+ */
+public class Buildex {
+    private static final Logger logger = LogManager.getLogger(Buildex.class);
+
+    public static void main(String[] args) {
+        logger.error("Hello KRV");
+    }
+}

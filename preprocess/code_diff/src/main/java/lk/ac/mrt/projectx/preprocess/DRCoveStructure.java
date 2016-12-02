37e1ff305ffaf5daa2b147c9df54184cf47e798a
KRVPerera <rukshan.viduranga@gmail.com>
2016-12-02 21:25:06 +0530

    file reading upper secion done

--- preprocess/code_diff/src/main/java/lk/ac/mrt/projectx/preprocess/DRCoveStructure.java
+++ preprocess/code_diff/src/main/java/lk/ac/mrt/projectx/preprocess/DRCoveStructure.java
@@ -1,3 +1,4 @@
+
 package lk.ac.mrt.projectx.preprocess;
 
 
@@ -6,12 +7,21 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * @author krv
+ * @version 0.9
+ *
+ */
 public class DRCoveStructure {
-
+    /**
+     * @see
+     */
     final static Logger logger = LogManager.getLogger(DRCoveStructure.class);
 
     Integer noOfBasicBlocks;
@@ -20,22 +30,57 @@ public class DRCoveStructure {
     ArrayList<Integer> duplicateIndexes;
     ArrayList<Integer> locateOriginals;
     Integer drcovVersion;
+    String drcovFlavour;
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        DRCoveStructure dd = new DRCoveStructure();
+        dd.LoadFromFile("/home/krv/Projects/FYP/project-x/preprocess/code_diff/test/drcov.halide_blur_hvscan_test.exe.02112.0000.proc.log");
     }
 
     public void LoadFromFile(String fileName){
         BufferedReader bufferedReader = null;
         FileReader fileReader = null;
         String currentLine;
+        Integer index = 0;
         try {
             fileReader = new FileReader(fileName);
             bufferedReader = new BufferedReader(fileReader);
 
+            // Read the first line which indicates DRCov version
+            // eg : DRCOV VERSION: 1
+            currentLine = bufferedReader.readLine();
+            logger.debug("First line",currentLine);
+            Matcher result = Pattern.compile("\\d+").matcher(currentLine);
+            if(result.find()) {
+                drcovVersion = Integer.parseInt(result.group().toString());
+            }
+            logger.debug("DRCov version ", drcovVersion);
+
+            // Only version 2 files include flavour line
+            // eg :
+            if(drcovVersion == 2){
+                currentLine = bufferedReader.readLine();
+                result = Pattern.compile("\\d+").matcher(currentLine);
+                if(result.find()) {
+                    drcovFlavour = result.group().toString();
+                }
+                logger.debug("DRCov Flavour ", currentLine);
+            }
+
+            // Get the number of modules
+            // eg : Module Table: 24
+            currentLine = bufferedReader.readLine();
+            result = Pattern.compile("\\d+").matcher(currentLine);
+            if(result.find()) {
+                noOfModules = Integer.parseInt(result.group().toString());
+//                System.out.println(noOfModules.toString());
+            }
+            logger.debug("Number of modules ", noOfModules);
+
             while ((currentLine = bufferedReader.readLine()) != null) {
+
                 System.out.println(currentLine);
+                index++;
             }
 
 

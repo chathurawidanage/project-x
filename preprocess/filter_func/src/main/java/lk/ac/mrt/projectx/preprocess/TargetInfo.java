9c34f51187ebb954553f3ff2ca6c7e2c88655331
Lasantha Ekanayake <mdslasantha@gmail.com>
2016-12-04 14:46:31 +0530

    TragetInfo class added

---
+++ preprocess/filter_func/src/main/java/lk/ac/mrt/projectx/preprocess/TargetInfo.java
@@ -0,0 +1,26 @@
+package lk.ac.mrt.projectx.preprocess;
+
+/**
+ * Created by Lasantha on 04-Dec-16.
+ */
+public class TargetInfo {
+
+    private int target;
+    private int frequency;
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public void setTarget(int target) {
+        this.target = target;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+}

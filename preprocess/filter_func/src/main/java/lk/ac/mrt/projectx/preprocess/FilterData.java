18c233498f15bc7d95ff2e39565c2a1cabb39502
Lasantha Ekanayake <mdslasantha@gmail.com>
2016-12-04 14:46:53 +0530

    FilterData class added

---
+++ preprocess/filter_func/src/main/java/lk/ac/mrt/projectx/preprocess/FilterData.java
@@ -0,0 +1,16 @@
+package lk.ac.mrt.projectx.preprocess;
+
+/**
+ * Created by Lasantha on 03-Dec-16.
+ */
+public class FilterData {
+    private String exeFileName;
+
+    public String getExeFileName() {
+        return exeFileName;
+    }
+
+    public void setExeFileName(String exeFileName) {
+        this.exeFileName = exeFileName;
+    }
+}

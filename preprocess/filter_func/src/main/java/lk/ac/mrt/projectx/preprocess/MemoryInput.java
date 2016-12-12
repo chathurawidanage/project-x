b8f074af1ac47289e83210a3a606a061e154d5d1
psynd2 <psynd2@exmail.nottingham.ac.uk>
2016-12-12 11:51:12 +0530

    MemoryInput class added

---
+++ preprocess/filter_func/src/main/java/lk/ac/mrt/projectx/preprocess/MemoryInput.java
@@ -0,0 +1,64 @@
+package lk.ac.mrt.projectx.preprocess;
+
+/**
+ * Created by Lasantha on 12-Dec-16.
+ */
+public class MemoryInput {
+
+
+    private String module;
+    private int pc;
+
+    private long memAddress;
+    private boolean write;
+    private int stride;
+    private int type;
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public int getPc() {
+        return pc;
+    }
+
+    public void setPc(int pc) {
+        this.pc = pc;
+    }
+
+    public long getMemAddress() {
+        return memAddress;
+    }
+
+    public void setMemAddress(long memAddress) {
+        this.memAddress = memAddress;
+    }
+
+    public boolean isWrite() {
+        return write;
+    }
+
+    public void setWrite(boolean write) {
+        this.write = write;
+    }
+
+    public int getStride() {
+        return stride;
+    }
+
+    public void setStride(int stride) {
+        this.stride = stride;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}

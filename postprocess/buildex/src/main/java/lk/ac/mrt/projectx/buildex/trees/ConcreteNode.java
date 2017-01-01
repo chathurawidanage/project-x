461a6bf61685debbeb4d56cb84c9bdd59990bfa5
KRVPerera <rukshan.viduranga@gmail.com>
2017-01-01 14:31:37 +0530

    ConcreteNode comparable

--- postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/trees/ConcreteNode.java
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/trees/ConcreteNode.java
@@ -1,8 +1,71 @@
 package lk.ac.mrt.projectx.buildex.trees;
 
+import com.sun.org.apache.regexp.internal.RE;
+import lk.ac.mrt.projectx.buildex.MemoryRegion;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by krv on 1/1/17.
  */
-public class ConcreteNode {
+public class ConcreteNode <T> extends Node <T> implements Comparable{
+
+    //region pubclic constructors
+
+
+    //endregion pubclic constructors
+
+    //region private variables
+
+    private MemoryRegion region;
+
+    //endregion private variables
+
+    //region public methods
+
+    //endregion public methods
+
+    //region overridden methods
+
+    @Override
+    public int compareTo(Object o) {
+        ConcreteNode node = (ConcreteNode) o;
+        if(!node.srcs.isEmpty()){
+            return (this.operation == node.operation) &&
+                    (this.srcs.size() == node.srcs.size()) ? 1 : 0;
+        }else{
+            if(symbol.type == node.symbol.type){
+                if(symbol.type == Operand.OperandType.IMM_INT_TYPE){
+                    return 1;
+                }else if(symbol.type == Operand.OperandType.IMM_FLOAT_TYPE){
+                    return Float.compare((Float)symbol.value, (Float)node.symbol.value);
+                }else{
+                    return 1;
+                }
+            }else{
+                return 0;
+            }
+        }
+    }
+
+    @Override
+    public String getNodeString() {
+        return null;
+    }
+
+    @Override
+    public String getDotString() {
+        return getDotString();
+    }
+
+    @Override
+    public String getSimpleString() {
+        throw new NotImplementedException();
+    }
+
+    //endregion overridden methods
+
+    //region private methods
+
+    //endregion private methods
 
 }

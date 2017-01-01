cdb9c6a19ed5cd4ca1fffe0988c01870c377819d
KRVPerera <rukshan.viduranga@gmail.com>
2017-01-01 12:04:21 +0530

    getMemString - implemented

--- postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/trees/AbstractNode.java
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/trees/AbstractNode.java
@@ -7,6 +7,7 @@ import sun.invoke.empty.Empty;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.Math.abs;
 
@@ -39,7 +40,6 @@ public class AbstractNode <T> extends Node<T> implements Comparable {
 
     //endregion private variables
 
-
     //region public methods
 
     public String GetMemString() {
@@ -53,6 +53,10 @@ public class AbstractNode <T> extends Node<T> implements Comparable {
         memSt.append(this.pos.get(this.dimensions - 1));
         memSt.append(")");
         return memSt.toString();
+    }
+
+    public String GetSymbolicString(List<String> vars) {
+        throw new NotImplementedException();
     }
 
     //region overridden methods
@@ -87,14 +91,14 @@ public class AbstractNode <T> extends Node<T> implements Comparable {
 
     @Override
     public String getNodeString() {
-        if(this.type == AbstractNodeType.INPUT_NODE || this.type == AbstractNodeType.OUTPUT_NODE
-                || this.type == AbstractNodeType.INTERMEDIATE_NODE){
+        if (this.type == AbstractNodeType.INPUT_NODE || this.type == AbstractNodeType.OUTPUT_NODE
+                || this.type == AbstractNodeType.INTERMEDIATE_NODE) {
             return GetMemString();
-        }else if(this.type == AbstractNodeType.IMMEDIATE_INT || this.type == AbstractNodeType.IMMEDIATE_FLOAT){
+        } else if (this.type == AbstractNodeType.IMMEDIATE_INT || this.type == AbstractNodeType.IMMEDIATE_FLOAT) {
             return this.symbol.value.toString();
-        }else if(this.type == AbstractNodeType.OPERATION_ONLY){
+        } else if (this.type == AbstractNodeType.OPERATION_ONLY) {
             return this.operation.toString();
-        }else if(this.type == AbstractNodeType.PARAMETER){
+        } else if (this.type == AbstractNodeType.PARAMETER) {
             return "p_" + this.para_num.toString();
         }
         return "";
@@ -111,4 +115,54 @@ public class AbstractNode <T> extends Node<T> implements Comparable {
     }
     //endregion overridden methods
     //endregion public methods
+
+    //region private methods
+    private String getMemString(List<String> vars) {
+        StringBuilder retSt = new StringBuilder();
+        String in_string = (this.associatedMem.getDirection() == MemoryRegion.Direction.MEM_OUTPUT) ? "_buf_in" : "";
+
+        retSt.append(this.associatedMem.getName());
+        retSt.append(in_string);
+        retSt.append("(");
+        boolean first = true;
+
+        for (int i = 0 ; i < this.dimensions ; i++) {
+            for (int j = 0 ; j < this.headDiemensions ; j++) {
+                if (this.indexes.get(i).get(j) == 1) {
+                    if (!first) {
+                        retSt.append("+");
+                    }
+                    retSt.append(vars.get(j));
+                    first = false;
+                } else if (this.indexes.get(i).get(j) != 0) {
+                    if (!first) {
+                        retSt.append("+");
+                    }
+                    retSt.append("(");
+                    retSt.append(this.indexes.get(i).get(j).toString());
+                    retSt.append(")");
+                    retSt.append("*");
+                    retSt.append(vars.get(j));
+                    first = false;
+
+                }
+            }
+
+            if (!first) {
+                if (this.indexes.get(i).get(this.headDiemensions) != 0) {
+                    retSt.append("+");
+                    retSt.append(this.indexes.get(i).get(this.headDiemensions).toString());
+                }
+            } else {
+                retSt.append(this.indexes.get(i).get(this.headDiemensions).toString());
+            }
+
+            if (this.dimensions - 1 != i) {
+                retSt.append(",");
+            } else {
+                retSt.append(")");
+            }
+        }
+        return retSt.toString();
+    }
 }

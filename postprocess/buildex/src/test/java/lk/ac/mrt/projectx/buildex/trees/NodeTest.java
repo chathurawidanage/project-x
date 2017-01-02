789105010d7d4a7bfed3aa4af89e45bcb8e37cd4
KRVPerera <rukshan.viduranga@gmail.com>
2017-01-02 10:03:40 +0530

    node test suit init

---
+++ postprocess/buildex/src/test/java/lk/ac/mrt/projectx/buildex/trees/NodeTest.java
@@ -0,0 +1,20 @@
+package lk.ac.mrt.projectx.buildex.trees;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by krv on 1/2/17.
+ */
+public class NodeTest extends TestCase {
+
+    public void testIsNodeSimilar() throws Exception {
+        AbstractNode<Float> node1 = new AbstractNode<>();
+        AbstractNode<Float> node2 = new AbstractNode<>(node1);
+
+    }
+
+    public void testIsNodesSimilar() throws Exception {
+
+    }
+
+}

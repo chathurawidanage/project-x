0c4e81baf4f125cae269ed91987fce2750581544
KRVPerera <rukshan.viduranga@gmail.com>
2017-01-02 07:30:10 +0530

    Operant test for constructor float and integer

--- postprocess/buildex/src/test/java/lk/ac/mrt/projectx/buildex/trees/OperandTest.java
+++ postprocess/buildex/src/test/java/lk/ac/mrt/projectx/buildex/trees/OperandTest.java
@@ -7,6 +7,19 @@ import junit.framework.TestCase;
  */
 public class OperandTest extends TestCase {
 
+    public void testConstructorInteger() {
+        Operand<Integer> operand = new Operand<>(Operand.OperandType.IMM_INT_TYPE, 50, 160);
+
+        assertEquals((Integer)50, operand.value);
+    }
+
+    public void testConstructorFloat() {
+        Operand<Float> operand = new Operand<>(Operand.OperandType.IMM_FLOAT_TYPE, 50.0f, 160);
+
+        assertEquals(50.0f, operand.value);
+    }
+
+
     public void testMemRangeToRegName() {
         Operand<Integer> operand = new Operand<>();
         operand.type = Operand.OperandType.REG_TYPE;

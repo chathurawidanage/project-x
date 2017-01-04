6efa61e7a5860126811844b8ed879d64800ed11f
KRVPerera <rukshan.viduranga@gmail.com>
2017-01-04 18:19:02 +0530

    merge conflict fix on Node classes

--- postprocess/buildex/src/test/java/lk/ac/mrt/projectx/buildex/trees/NodeTest.java
+++ postprocess/buildex/src/test/java/lk/ac/mrt/projectx/buildex/trees/NodeTest.java
@@ -9,13 +9,8 @@ public class NodeTest extends TestCase {
 
     public void testIsNodeSimilar() throws Exception {
         AbstractNode<Float> node1 = new AbstractNode<>();
-<<<<<<< HEAD
         AbstractNode<Float> node2 = new AbstractNode<>(node1);
         int ans = node1.compareTo(node2);
         assertEquals(1,ans);
-=======
-        AbstractNode<Float> node2 = new AbstractNode<>();
-
->>>>>>> cf85ef59cacdd4e1e92e69c254b0d53f19919468
     }
 }

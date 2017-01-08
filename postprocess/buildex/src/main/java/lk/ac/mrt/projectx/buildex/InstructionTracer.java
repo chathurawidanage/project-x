dc1cbfe928e80556346d795e95159337d938aefa
KRVPerera <rukshan.viduranga@gmail.com>
2017-01-08 21:44:33 +0530

    commenting error lines of last commit

--- postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/InstructionTracer.java
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/InstructionTracer.java
@@ -329,7 +329,7 @@ public class InstructionTracer {
                 for (int j = 0; j < instruction.getNum_srcs(); j++) {
                     if (instruction.srcs.get(j).getType() == InstructionTraceUnit.REG_TYPE &&
                             (long)instruction.srcs.get(j).getValue() == 0) {
-                        instruction.srcs.get(j).se
+                        //instruction.srcs.get(j).se
                     }
                 }
             }

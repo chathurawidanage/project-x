feb8bf04ad8af7c15c583a5dfbb8d60bd5cc376f
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-10 00:14:00 +0530

    defining Operand Type constants

--- postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/models/output/Operand.java
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/models/output/Operand.java
@@ -4,6 +4,13 @@ package lk.ac.mrt.projectx.buildex.models.output;
  * @author Chathura Widanage
  */
 public class Operand {
+    public static final int REG_TYPE = 0;
+    public static final int MEM_STACK_TYPE = 1;
+    public static final int MEM_HEAP_TYPE = 2;
+    public static final int IMM_FLOAT_TYPE = 3;
+    public static final int IMM_INT_TYPE = 4;
+    public static final int DEFAULT_TYPE = 5;
+
     private int type;
     private int width;
     private double value;

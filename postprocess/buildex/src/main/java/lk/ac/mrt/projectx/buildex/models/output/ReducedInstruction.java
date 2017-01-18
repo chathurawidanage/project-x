7271ae90de6b53b01e93980c33218da5347b3ce3
KRVPerera <rukshan.viduranga@gmail.com>
2017-01-18 09:11:41 +0530

    DrDefines EFLAGS_BITS enum

--- postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/models/output/ReducedInstruction.java
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/models/output/ReducedInstruction.java
@@ -2,6 +2,8 @@ package lk.ac.mrt.projectx.buildex.models.output;
 
 import lk.ac.mrt.projectx.buildex.trees.Operand;
 import lk.ac.mrt.projectx.buildex.x86.X86Analysis.Operation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
@@ -10,6 +12,7 @@ import java.util.List;
  */
 public class RInstruction {
 
+    final static Logger logger = LogManager.getLogger(RInstruction.class);
     //region Private variables
 
     private Operation operation;
@@ -17,7 +20,7 @@ public class RInstruction {
     private List<Operand> srcs;
     private Boolean sign;
     private Boolean isFloating;
-
+    private Integer numRinstr;
     //endregion Private variables
 
     //region Public methods
@@ -74,4 +77,25 @@ public class RInstruction {
     }
 
     //endregion Public methods
+
+    //region Private methods
+
+    /**
+     *  Pure function without side effects for cinstr (output)
+     *  TODO : check to make sure
+     * @param cinst - pointer to a pre-populated complex x86 instruction
+     * @param disams - string of diassembly of 'cinstr'. Can be used for debugging purposes
+     * @param line - the line in which this instruction can be found in the instruction trace (after filtering)
+     * @return amount - how many reduced set instruction were needed to canonicalize the given x86 instruction
+     */
+    private Integer cinstrToRinstr(Output cinst, String disams, Integer line){
+        logger.debug("Enter canonicalization - app_pc");
+        Integer amount = 0;
+        RInstruction rInstruction = new RInstruction();
+
+        //lets see whether this works
+
+        return amount;
+    }
+    //endregion Private methods
 }

4928337154a4ab5ed4380b846a52fb602ecdde2a
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-22 17:12:30 +0530

    AbstractTree missing attributes

--- postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/trees/AbstractTree.java
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/trees/AbstractTree.java
@@ -1,9 +1,24 @@
 package lk.ac.mrt.projectx.buildex.trees;
 
+import lk.ac.mrt.projectx.buildex.models.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by krv on 1/2/17.
+ * @author Chathura Widanage
+ * @author Rukshan Perera
  */
 public class AbstractTree extends Tree {
+    private List<Pair<AbstractTree,Boolean>> conditionalTrees=new ArrayList<>();
+
+    public List<Pair<AbstractTree, Boolean>> getConditionalTrees() {
+        return conditionalTrees;
+    }
+
+    public void setConditionalTrees(List<Pair<AbstractTree, Boolean>> conditionalTrees) {
+        this.conditionalTrees = conditionalTrees;
+    }
 
     @Override
     public void simplifyTree() {

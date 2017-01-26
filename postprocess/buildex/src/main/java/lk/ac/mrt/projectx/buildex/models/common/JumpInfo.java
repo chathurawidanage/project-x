b5454b2edb76d7e804de4938357a6212648a4425
KRVPerera <rukshan.viduranga@gmail.com>
2017-01-26 20:19:27 +0530

    JumpInfo - getters and setters

--- postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/models/common/JumpInfo.java
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/models/common/JumpInfo.java
@@ -46,4 +46,60 @@ public class JumpInfo {
         inst.not_taken = not_taken;
         return inst;
     }
+
+    public long getJump_pc() {
+        return jump_pc;
+    }
+
+    public void setJump_pc(long jump_pc) {
+        this.jump_pc = jump_pc;
+    }
+
+    public long getCond_pc() {
+        return cond_pc;
+    }
+
+    public void setCond_pc(long cond_pc) {
+        this.cond_pc = cond_pc;
+    }
+
+    public long getTarget_pc() {
+        return target_pc;
+    }
+
+    public void setTarget_pc(long target_pc) {
+        this.target_pc = target_pc;
+    }
+
+    public long getFall_pc() {
+        return fall_pc;
+    }
+
+    public void setFall_pc(long fall_pc) {
+        this.fall_pc = fall_pc;
+    }
+
+    public long getMerge_pc() {
+        return merge_pc;
+    }
+
+    public void setMerge_pc(long merge_pc) {
+        this.merge_pc = merge_pc;
+    }
+
+    public long getTaken() {
+        return taken;
+    }
+
+    public void setTaken(long taken) {
+        this.taken = taken;
+    }
+
+    public long getNot_taken() {
+        return not_taken;
+    }
+
+    public void setNot_taken(long not_taken) {
+        this.not_taken = not_taken;
+    }
 }

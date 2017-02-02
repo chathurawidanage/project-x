7c1b202ce12611cbe44ac8f358c8218411d01dba
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-02-02 10:32:32 +0530

    Coordinate classes implementation

---
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/complex/cordinates/PolarCoordinate.java
@@ -0,0 +1,29 @@
+package lk.ac.mrt.projectx.buildex.complex.cordinates;
+
+/**
+ * @author Chathura Widanage
+ */
+public class PolarCoordinate {
+    private double theta;
+    private double r;
+    public PolarCoordinate(double theta, double r){
+        this.theta=theta;
+        this.r=r;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+}

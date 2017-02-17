6f688eb7bd8c1a2cd29e9dcb8f5192c46565e93b
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-02-17 14:45:03 +0530

    Polar filter cordinate generation implementation

---
+++ postprocess/buildex/src/main/java/lk/ac/mrt/projectx/buildex/complex/generators/PolarGenerator.java
@@ -0,0 +1,35 @@
+package lk.ac.mrt.projectx.buildex.complex.generators;
+
+import lk.ac.mrt.projectx.buildex.complex.CoordinateTransformer;
+import lk.ac.mrt.projectx.buildex.complex.cordinates.CartesianCoordinate;
+import lk.ac.mrt.projectx.buildex.complex.cordinates.PolarCoordinate;
+import lk.ac.mrt.projectx.buildex.models.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Chathura Widanage
+ */
+public class PolarGenerator extends Generator {
+    public List<Pair<CartesianCoordinate, CartesianCoordinate>> generate(int width, int height) {
+        List<Pair<CartesianCoordinate, CartesianCoordinate>> examples = new ArrayList<>();
+        double maxR = Math.hypot(width / 2, height / 2);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                CartesianCoordinate cartesianCoordinate = new CartesianCoordinate(i, j);
+                PolarCoordinate polarCoordinate = CoordinateTransformer.cartesian2Polar(width, height, cartesianCoordinate);
+
+                CartesianCoordinate newCartCord = new CartesianCoordinate(polarCoordinate.getTheta() * width / (Math.PI * 4), polarCoordinate.getR() * height / maxR);
+                CoordinateTransformer.cartesianToCorner(width, height, newCartCord);
+                if (clampPass(width, height, newCartCord)) {
+                    examples.add(new Pair<>(
+                            cartesianCoordinate,
+                            newCartCord
+                    ));
+                }
+            }
+        }
+        return examples;
+    }
+}
